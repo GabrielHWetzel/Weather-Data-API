97ba1f428951c310ccc38a3352640253c6a6ca8f
Gabriel Henrique Wetzel <gabrielhwetzel@gmail.com>
2024-03-22 10:26:52 -0300

    Rendering dynamic page based on user input

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from flask import Flask, render_template
 
-app = Flask("Website")
+app = Flask(__name__)
 
 
 @app.route("/")
-@app.route("/home")
 def home():
     return render_template("Home.html")
 
 
-@app.route("/about")
-def about():
-    return render_template("About.html")
+@app.route("/api/v1/<station>/<date>")
+def api(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
 
 
-@app.route("/contact")
-def contact():
-    return render_template("Contact.html")
-
-
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
